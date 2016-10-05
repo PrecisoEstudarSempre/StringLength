9800e595074a24cd04b936f6f4324cdf5d4eceae
Preciso Estudar Sempre <precisoestudarsempre@gmail.com>
2016-10-05 17:56:35 -0300

    Realização de testes

--- StringLength/src/StringLength.java
+++ StringLength/src/StringLength.java
@@ -1,19 +1,38 @@
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+
 class StringLength {
 	public static void main(String[] args) {
-		String s = "joão";
+		String s = "ãããã";
 		System.out.println("Length: " + s.length());
 		
 		try {
-			System.out.println("Bytes: " + s.getBytes("ISO-8859-1").length);
+			System.out.println("Bytes em ISO-8859-1: " + s.getBytes("ISO-8859-1").length);
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
 
 		try {
-			System.out.println("Bytes: " + s.getBytes("UTF-8").length);
+			System.out.println("Bytes em UTF-8: " + s.getBytes("UTF-8").length);
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
+
+		try {
+			System.out.println("Bytes em UTF-16: " + s.getBytes("UTF-16").length);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			System.out.println("Bytes em windows-1252: " + s.getBytes("windows-1252").length);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Bytes em chamada de getBytes sem argumentos: " + s.getBytes().length);
+
+		System.out.println("Default Charset da nossa JVM: "+ Charset.defaultCharset());
 	}
 	
 }
