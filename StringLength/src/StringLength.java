c61f71fd2a2617374cd4c3349b32a55f995bbfcd
Preciso Estudar Sempre <precisoestudarsempre@gmail.com>
2016-10-12 19:56:57 -0300

    Alterações para testes

--- StringLength/src/StringLength.java
+++ StringLength/src/StringLength.java
@@ -3,7 +3,9 @@ import java.nio.charset.Charset;
 
 class StringLength {
 	public static void main(String[] args) {
-		String s = "ãããã";
+		
+		String s = "joão";
+		/*
 		System.out.println("Length: " + s.length());
 		
 		try {
@@ -33,6 +35,19 @@ class StringLength {
 		System.out.println("Bytes em chamada de getBytes sem argumentos: " + s.getBytes().length);
 
 		System.out.println("Default Charset da nossa JVM: "+ Charset.defaultCharset());
+
+		char c = 7;
+		System.out.println(c);
+		char a = 'a';
+		System.out.println(Integer.toHexString(a));
+		*/
+
+		System.out.println(s.charAt(4) + "-" + s.codePointAt(4));
+		char[] array = s.toCharArray();
+		System.out.println(array.length);
+		for (char c : array) {
+			System.out.print(c);
+		}
 	}
 	
 }
