0892389c82ed314f704d62d1d93a68827b2b3fb0
Preciso Estudar Sempre <precisoestudarsempre@gmail.com>
2016-10-14 18:14:23 -0300

    Testes

--- StringLength/src/StringLength.java
+++ StringLength/src/StringLength.java
@@ -1,19 +1,9 @@
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
+import java.io.OutputStreamWriter;
 
 class StringLength {
 	public static void main(String[] args) {		
-		String stringDefaultCharset = "joão";
-		System.out.println("Length em default charset: " + stringDefaultCharset.length());
-		System.out.println("Length levando em conta a quantidade de caracteres: " + stringDefaultCharset.codePointCount(0,stringDefaultCharset.length()));
-		System.out.println("Default Charset da nossa JVM: "+ Charset.defaultCharset());
-
-		String stringUTF8 = new String(stringDefaultCharset.getBytes(),Charset.forName("UTF-8"));
-		System.out.println("Length em UTF-8: " + stringUTF8.length());
-		System.out.println("Length levando em conta a quantidade de caracteres: " + stringUTF8.codePointCount(0,stringUTF8.length()));
-
-		String stringISO88591 = new String(stringDefaultCharset.getBytes(),Charset.forName("ISO-8859-1"));
-		System.out.println("Length em ISO 8859-1: " + stringISO88591.length());
-		System.out.println("Length levando em conta a quantidade de caracteres: " + stringISO88591.codePointCount(0,stringISO88591.length()));
+		//System.out.println("joão");
+		OutputStreamWriter osw = new OutputStreamWriter(System.out);
+		System.out.println(osw.getEncoding());
 	}	
 }
