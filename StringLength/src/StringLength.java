b697221173fadcef10d5537714e69ec7c433d88e
Preciso Estudar Sempre <precisoestudarsempre@gmail.com>
2016-10-15 22:51:58 -0300

    Alterações finais
    Alteração final na classe de teste e remoção do arquivo compilado.

--- StringLength/src/StringLength.java
+++ StringLength/src/StringLength.java
@@ -1,9 +1,7 @@
-import java.io.OutputStreamWriter;
-
 class StringLength {
 	public static void main(String[] args) {		
-		//System.out.println("joão");
-		OutputStreamWriter osw = new OutputStreamWriter(System.out);
-		System.out.println(osw.getEncoding());
+		String s = "joão";
+		System.out.println("Nossa string:" + s);
+		System.out.println("O tamanho(length) da nossa string: " + s.length());
 	}	
 }
